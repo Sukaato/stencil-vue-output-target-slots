31daae5ce87b94024efbc74962f87599fcaf7b33
Sukaato <sukaato.dev@proton.me>
2024-06-18 09:18:31 +0200

    Create projet for bug reproduction purpose

---
+++ src/views/HomePage.vue
@@ -0,0 +1,62 @@
+<script setup lang="ts">
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonButton, IonIcon } from '@ionic/vue';
+</script>
+
+<template>
+  <IonPage>
+    <IonHeader :translucent="true">
+      <IonToolbar>
+        <IonTitle>Blank</IonTitle>
+      </IonToolbar>
+    </IonHeader>
+
+    <IonContent :fullscreen="true">
+      <IonHeader collapse="condense">
+        <IonToolbar>
+          <IonTitle size="large">Blank</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+
+      <div id="container">
+        <IonButton>
+          <template #default>Button test</template>
+          <!-- #start , #end & #icon-only are not showns in autocomplete -->
+          <template #start>
+            <IonIcon icon="star" />
+          </template>
+        </IonButton>
+      </div>
+    </IonContent>
+  </IonPage>
+</template>
+
+
+<style scoped>
+#container {
+  text-align: center;
+  
+  position: absolute;
+  left: 0;
+  right: 0;
+  top: 50%;
+  transform: translateY(-50%);
+}
+
+#container strong {
+  font-size: 20px;
+  line-height: 26px;
+}
+
+#container p {
+  font-size: 16px;
+  line-height: 22px;
+  
+  color: #8c8c8c;
+  
+  margin: 0;
+}
+
+#container a {
+  text-decoration: none;
+}
+</style>
